b384e4c633d8d81fb2d24be6538d2ef38a045442
AshwiniRev <ashwini@revinfotech.com>
2025-11-06 13:54:42 +0530

    init

---
+++ src/v2/mappings/factory.ts
@@ -0,0 +1,142 @@
+/* eslint-disable prefer-const */
+import { BigInt } from "@graphprotocol/graph-ts";
+import { log } from "@graphprotocol/graph-ts";
+
+import { PairCreated } from "../../../generated/Factory/Factory";
+import {
+  Bundle,
+  Pair,
+  PairTokenLookup,
+  Token,
+  UniswapFactory,
+} from "../../../generated/schema";
+import { Pair as PairTemplate } from "../../../generated/templates";
+import { FACTORY_ADDRESS } from "../../common/chain";
+import { ZERO_BD, ZERO_BI } from "../../common/constants";
+import {
+  fetchTokenDecimals,
+  fetchTokenName,
+  fetchTokenSymbol,
+  fetchTokenTotalSupply,
+} from "../../common/helpers";
+
+export function handleNewPair(event: PairCreated): void {
+  // Load factory (create if first exchange)
+  let factory = UniswapFactory.load(FACTORY_ADDRESS);
+  if (factory === null) {
+    factory = new UniswapFactory(FACTORY_ADDRESS);
+    factory.pairCount = ZERO_BI;
+    factory.totalVolumeETH = ZERO_BD;
+    factory.totalLiquidityETH = ZERO_BD;
+    factory.totalVolumeUSD = ZERO_BD;
+    factory.untrackedVolumeUSD = ZERO_BD;
+    factory.totalLiquidityUSD = ZERO_BD;
+    factory.txCount = ZERO_BI;
+
+    // Create new bundle
+    let bundle = new Bundle("1");
+    bundle.ethPriceUSD = ZERO_BD;
+    bundle.save();
+  }
+
+  factory.pairCount = factory.pairCount.plus(BigInt.fromI32(1));
+  factory.save();
+
+  // Create tokens
+  let token0 = Token.load(event.params.token0.toHexString());
+  let token1 = Token.load(event.params.token1.toHexString());
+
+  // Fetch info if null
+  if (token0 === null) {
+    token0 = new Token(event.params.token0.toHexString());
+    token0.symbol = fetchTokenSymbol(event.params.token0);
+    token0.name = fetchTokenName(event.params.token0);
+    token0.totalSupply = fetchTokenTotalSupply(event.params.token0);
+    let decimals = fetchTokenDecimals(event.params.token0);
+
+    if (decimals === null) {
+      log.debug("mybug: the decimals on token0 was null", []);
+      return;
+    }
+
+    token0.decimals = decimals;
+    token0.derivedETH = ZERO_BD;
+    token0.tradeVolume = ZERO_BD;
+    token0.tradeVolumeUSD = ZERO_BD;
+    token0.untrackedVolumeUSD = ZERO_BD;
+    token0.totalLiquidity = ZERO_BD;
+    token0.txCount = ZERO_BI;
+    token0.whitelistPairs = [];
+  }
+
+  if (token1 === null) {
+    token1 = new Token(event.params.token1.toHexString());
+    token1.symbol = fetchTokenSymbol(event.params.token1);
+    token1.name = fetchTokenName(event.params.token1);
+    token1.totalSupply = fetchTokenTotalSupply(event.params.token1);
+    let decimals = fetchTokenDecimals(event.params.token1);
+
+    if (decimals === null) {
+      log.debug("mybug: the decimals on token1 was null", []);
+      return;
+    }
+
+    token1.decimals = decimals;
+    token1.derivedETH = ZERO_BD;
+    token1.tradeVolume = ZERO_BD;
+    token1.tradeVolumeUSD = ZERO_BD;
+    token1.untrackedVolumeUSD = ZERO_BD;
+    token1.totalLiquidity = ZERO_BD;
+    token1.txCount = ZERO_BI;
+    token1.whitelistPairs = [];
+  }
+
+  // Create pair entity
+  let pair = new Pair(event.params.pair.toHexString());
+  pair.token0 = token0.id;
+  pair.token1 = token1.id;
+  pair.liquidityProviderCount = ZERO_BI;
+  pair.createdAtTimestamp = event.block.timestamp;
+  pair.createdAtBlockNumber = event.block.number;
+  pair.txCount = ZERO_BI;
+  pair.reserve0 = ZERO_BD;
+  pair.reserve1 = ZERO_BD;
+  pair.trackedReserveETH = ZERO_BD;
+  pair.reserveETH = ZERO_BD;
+  pair.reserveUSD = ZERO_BD;
+  pair.totalSupply = ZERO_BD;
+  pair.volumeToken0 = ZERO_BD;
+  pair.volumeToken1 = ZERO_BD;
+  pair.volumeUSD = ZERO_BD;
+  pair.untrackedVolumeUSD = ZERO_BD;
+  pair.token0Price = ZERO_BD;
+  pair.token1Price = ZERO_BD;
+
+  // Create the tracked contract based on the template
+  PairTemplate.create(event.params.pair);
+
+  // Save entities
+  token0.save();
+  token1.save();
+  pair.save();
+  factory.save();
+
+  // Pair lookup for fast querying
+  let pairLookup0 = new PairTokenLookup(
+    event.params.token0
+      .toHexString()
+      .concat("-")
+      .concat(event.params.token1.toHexString())
+  );
+  pairLookup0.pair = pair.id;
+  pairLookup0.save();
+
+  let pairLookup1 = new PairTokenLookup(
+    event.params.token1
+      .toHexString()
+      .concat("-")
+      .concat(event.params.token0.toHexString())
+  );
+  pairLookup1.pair = pair.id;
+  pairLookup1.save();
+}
